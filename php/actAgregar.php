26951d7d587dbfb9ee72a1921b06b3e8fc82e162
José Camacho <57728112+JDcamacho13@users.noreply.github.com>
2020-12-16 13:38:29 -0400

    Hotfix formato de imágenes

--- php/actAgregar.php
+++ php/actAgregar.php
@@ -6,7 +6,8 @@ if(!productoExiste($_POST['nombre'])){
     if (($_FILES["file"]["type"] == "image/pjpeg")
         || ($_FILES["file"]["type"] == "image/jpeg")
         || ($_FILES["file"]["type"] == "image/png")
-        || ($_FILES["file"]["type"] == "image/gif")){
+        || ($_FILES["file"]["type"] == "image/gif")
+        || ($_FILES["file"]["type"] == "image/jpg")){
             if(agregarProducto($_POST['nombre'], $_POST['precio'], "images/".$_FILES['file']['name'])){
                 if (move_uploaded_file($_FILES["file"]["tmp_name"], "../images/".$_FILES['file']['name'])){
                 
